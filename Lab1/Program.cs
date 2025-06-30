e5c61fee701218b0860394dbf306b663ef463ddb
Coding-star890Liz <emailtomemgb@gmail.com>
2025-06-30 22:57:57 +0000

    Lab1 in progress, step 3 completed

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        //step 1: Comments
+        //Name: Medha Bhattacharyya
+        //Title: IT-1050 - Lab 1
+
+        // Step 2: Basic Output
+        Console.WriteLine("MedhaBhattacharyya");
+        Console.WriteLine("IT-1050");
+
+        //Step 3: Declare and Use Variables
+        int favoriteNumber = 7;
+        Console.WriteLine("My favorite number is: " + favoriteNumber);
+
+        string language = "python";
+        Console.WriteLine("My favorite programming language: " + language);
+
+        double programs = 163;
+        Console.WriteLine("The number of programs I have written is: " + programs);
+
+        bool programmingExp = true;
+        Console.WriteLine("Do you have programming experience? " + (programmingExp ? "Yes" : "No"));        
+    } 
 }
