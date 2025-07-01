849bae38db016174cd51907a7a88a11d53485d6b
Coding-star890Liz <emailtomemgb@gmail.com>
2025-07-01 01:19:27 +0000

    Lab1 in progress, step 5 completed

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -23,6 +23,31 @@ class Program
         Console.WriteLine("The number of programs I have written is: " + programs);
 
         bool programmingExp = true;
-        Console.WriteLine("Do you have programming experience? " + (programmingExp ? "Yes" : "No"));        
+        Console.WriteLine("Do you have programming experience? " + (programmingExp ? "Yes" : "No"));
+
+        //Step 4:  Use Constants    
+        const string schoolName = "Solon High School";
+        Console.WriteLine($"My school name: {schoolName}");
+
+        //Step 5: Type Casting
+        double myDouble = 9.78;
+        int myInt = (int)myDouble; // Explicit casting from double to int
+        Console.WriteLine($"Double type value {myDouble} Type Casting to integer {myInt}");
+
+        //Convert an int to a string
+        int myInt2 = 65;
+        Console.WriteLine($"Integer type value {myInt2} converted to string {Convert.ToString(myInt2)}");
+
+        //Convert a bool to a string
+        bool myBool = true;
+        Console.WriteLine($"Integer type value {myBool} converted to string {Convert.ToString(myBool)}");
+
+        //Print each value with a label
+        Console.WriteLine("myDouble: " + myDouble);
+        Console.WriteLine("myInt2: " + myInt2);
+        Console.WriteLine("myBool: " + myBool);
+
+        //Step 6: User Input and Type Conversion
+        
     } 
 }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51858a230d2a796a4d54240009d12df0a35d46b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c61fee701218b0860394dbf306b663ef463ddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
