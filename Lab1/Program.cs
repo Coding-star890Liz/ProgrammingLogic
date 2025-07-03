52fb9ecb5030f1c83211ffd5cd73a1b848f321f5
Coding-star890Liz <emailtomemgb@gmail.com>
2025-07-03 04:42:39 +0000

    Fine tuning the code for submission

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,15 +8,18 @@ class Program
     static void Main(string[] args)
     {
         // step 1: Comments
-        /*Name: Medha Bhattacharyya
+        // Comments are used to explain and document the code 
+        /*
+        Name: Medha Bhattacharyya
         Title: IT-1050 - Lab 1
         */
 
         // Step 2: Basic Output
-        Console.WriteLine("MedhaBhattacharyya");
+        //Console.WriteLine prints the text inside the parenthesis
+        Console.WriteLine("Medha Bhattacharyya");
         Console.WriteLine("IT-1050");
 
-        //Step 3: Declare and Use Variables
+        //Step 3: Declare and Use Variables: Declaring different data types and printing the values with labels       
         int favoriteNumber = 7;
         Console.WriteLine("My favorite number is: " + favoriteNumber);
 
@@ -29,96 +32,113 @@ class Program
         bool programmingExp = true;
         Console.WriteLine("Do you have programming experience? " + (programmingExp ? "Yes" : "No"));
 
-        //Step 4:  Use Constants    
+        //Step 4:  Use Constants: Declaring a constant string schoolName and printing it       
         const string schoolName = "Solon High School";
         Console.WriteLine($"My school name: {schoolName}");
 
-        //Step 5: Type Casting
+        //Step 5: Type Casting: Here we are checking different type casting for different data types
         double myDouble = 9.78;
-        int myInt = (int)myDouble; // Explicit casting from double to int
-        Console.WriteLine($"Double type value {myDouble} Type Casting to integer {myInt}");
+        int doubleToInt = (int)myDouble; // Explicit casting from double to int
+        Console.WriteLine($"Double type value {myDouble} Type Casting to integer {doubleToInt}");
 
-        //Convert an int to a string
+        //Convert an int to a string using Convert class
         int myInt2 = 65;
-        Console.WriteLine($"Integer type value {myInt2} converted to string {Convert.ToString(myInt2)}");
+        String intToString = Convert.ToString(myInt2);
+        Console.WriteLine($"Integer type value {myInt2} converted to string {intToString}");
 
-        //Convert a bool to a string
+        //Convert a bool to a string using Convert class
         bool myBool = true;
-        Console.WriteLine($"Integer type value {myBool} converted to string {Convert.ToString(myBool)}");
+        String boolToString = Convert.ToString(myBool);
+        Console.WriteLine($"Integer type value {myBool} converted to string {boolToString}");
 
-        //Print each value with a label
-        Console.WriteLine("myDouble: " + myDouble);
-        Console.WriteLine("myInt2: " + myInt2);
-        Console.WriteLine("myBool: " + myBool);
+        //Print each value with a label again
+        Console.WriteLine("doubleToInt: " + doubleToInt);
+        Console.WriteLine("intToString: " + intToString);
+        Console.WriteLine("boolToString: " + boolToString);
 
         //Step 6: User Input and Type Conversion
+        // This step prompts the user to eneter their name. It then prompts the user to enter their age 
+        // converting the string to an integer. Lastly, it displays a greeting that includes their name and age.
 
         //Prompts user to enter their name and store it in a string
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
 
-        //Prompts user to eneter their age and converts Console.ReadLine into Convery.ToInt2
+
+        //Prompts user to eneter their age and converts thet text string from the user to an integer
         Console.Write("Enter your age: ");
         int age = Convert.ToInt32(Console.ReadLine());
 
         //Display the greeting that includes their name and age
         Console.WriteLine($"Hello {name}, you are {age} years old!");
 
-        //Step 7: Arithmetic Operators
+        //Step 7: Arithmetic Operators: This shows various arithmetic operations and printing the output
 
-        //Assign int values
+        //Create two int variables called num1 and num2 and assign them values
         int num1 = 5;
         int num2 = 7;
 
         //Addition- add 10 for num1
-        Console.WriteLine("Addition for num1 is " + (num1 + 10));
+        Console.WriteLine("Add 10 to num1 is " + (num1 + 10));
 
         //Subtraction - subtract 2 for num1
-        Console.WriteLine("Subtraction for num1 is " + (num1 - 2));
+        Console.WriteLine("Subtract 2 from num1 is " + (num1 - 2));
 
         //Multiplication - times 3 for num1
-        Console.WriteLine("Multiplication for num1 is " + (num1 * 3));
+        Console.WriteLine("Multiply 3 to num1 is " + (num1 * 3));
 
         //Division - divide by 2 for num1
-        Console.WriteLine("Division for num2 is " + (num1 / 2));
+        Console.WriteLine("Divide 2 from num1 is " + (num1 / 2));
 
         //Modulus - modulus 2 for num1
         Console.WriteLine("Modulus for num1 is" + (num1 % 2));
 
         //Addition- add 10 for num2
-        Console.WriteLine("Addition for num2 is " + (num2 + 10));
+        Console.WriteLine("Add 10 to num2 is " + (num2 + 10));
 
         //Subtraction - subtract 2 for num2
-        Console.WriteLine("Subtraction for num2 is " + (num2 - 2));
+        Console.WriteLine("Subtract 2 from num2 is " + (num2 - 2));
 
         //Multiplication - times 3 for num2
-        Console.WriteLine("Multiplication for num2 is " + (num2 * 3));
+        Console.WriteLine("Multiply 3 to num2 is " + (num2 * 3));
 
         //Division - divide by 2 for num2
-        Console.WriteLine("Division for num2 is " + (num2 / 2));
+        Console.WriteLine("Divide 2 from num2 is " + (num2 / 2));
 
         //Modulus - modulus 2 for num2
         Console.WriteLine("Modulus for num2 is " + (num2 % 2));
 
         //Step 8: Floating Point Precision
 
-        //Create and assign both float and double variable
+        //Create and assign same value to both float and double variable
         double DoubleNum = 1.123456789D;
-        float DoubleNum2 = 1.123456789F;
+        float FloatNum2 = 1.123456789F;
 
         //Print both variables to the console
-        Console.WriteLine(DoubleNum);
-        Console.WriteLine(DoubleNum2);
-        Console.WriteLine("Double has a higher accuracy for the same number than float, so it recalls more decimal digits");
+        Console.WriteLine($"Double number printed for 1.123456789D is : {DoubleNum}");
+        Console.WriteLine($"Float number printed for 1.123456789F is : {FloatNum2}");
+
+        //Here getting the decimal digits length for each value
+        int decimalPlacesDouble = DoubleNum.ToString().Split('.')[1].Length;
+        int decimalPlacesFloat = FloatNum2.ToString().Split('.')[1].Length;
+        Console.WriteLine($"Double decimal digit length is: {decimalPlacesDouble}");
+        Console.WriteLine($"Float decimal digit length is: {decimalPlacesFloat}");
+
+        Console.WriteLine("Double has a higher precision for the same big decimal number than float, so it gets more precision");
+        Console.WriteLine("Here float print up to 7 decimal digits and rounds off after 7th decimal digit whereas double prints all decimal digits.");
+
+
         //Step 9: Increment and Decrement
+
+        //Declare an int variable and assign it a value of 10
         int x = 10;
 
         //Increment operator to increase value by 1
-        x++;
+        ++x;
         Console.WriteLine($"After Incremnt: {x} ");
 
         //Decrement operator to decrease value by 1
-        x--;
+        --x;
          Console.WriteLine($"After decrement: {x} ");
     } 
 }
