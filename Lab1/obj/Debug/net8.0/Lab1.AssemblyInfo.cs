b20b16870a56fff4b65b6bb73d27401b31a6a5de
Coding-star890Liz <emailtomemgb@gmail.com>
2025-07-05 03:16:58 +0000

    Updated

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54248010436a5c44db6450932acdd9ce5370982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccebe9aa638150fdd9fa871fab5b28c3692131e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace Lab2;
+﻿
+namespace Lab2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //Part 1:
+        int a = 10, b = 5, c = 15;
+
+        bool comparison1 = (a > b);
+        bool comparison2 = (a < c);
+        bool comparison3 = a > b && a > c;
+        bool comparison4 = a > b || a > c;
+
+        Console.WriteLine(comparison1);
+        Console.WriteLine(comparison2);
+        Console.WriteLine(comparison3);
+        Console.WriteLine(comparison4);
+
+
+        //Part 2:
+        bool isRaining = true;
+        bool haveUmbrella = false;
+
+        if (isRaining && !haveUmbrella)
+        {
+            Console.WriteLine("Take an umbrella!");
+        }
+        else if (isRaining == haveUmbrella)
+        {
+            Console.WriteLine("You are good to go!");
+         }
     }
 }
