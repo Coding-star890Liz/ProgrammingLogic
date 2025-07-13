0113ee193fe490efce8ba1d5b744278ec754b0a2
Coding-star890Liz <emailtomemgb@gmail.com>
2025-07-13 19:58:48 -0400

    Lab4 1st 2 problem.

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,9 +1,51 @@
 ﻿namespace Lab4;
 
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    // Example method to display car details.
+    public void Display()
+    {
+        Console.WriteLine("Car Details:");
+        Console.WriteLine("Model: " + model);
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("Year: " + year);
+    }
+
+    //Problem 1: Add a Method with No Parameters
+    public void Start()
+    {
+        Console.WriteLine("The Car is starting");
+    }
+
+    //Problem 2: Add a Method with Parameters
+    public void Drive(int miles)
+    {
+        Console.WriteLine($"The car drove {miles} miles");
+    }
+   
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        // Creating a Car object and assigning values to its properties
+        Car myCar = new Car();
+        myCar.model = "Civic";
+        myCar.color = "Black";
+        myCar.year = 2020;
+
+        myCar.Display(); // Calling the Display method
+
+        //Step 1 call method
+        myCar.Start();
+
+        //Step 2 call method
+        myCar.Drive(50);
     }
 }
+
